078092bccf8a817d911a74e1c1a14c7ae92447f4
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-06-14 23:30:26 -0300

    section 150 finished

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -11,16 +11,43 @@ export class MenuComponent implements OnInit {
   public menuItems: MenuItem[] = [];
 
   ngOnInit() {
-      this.menuItems = [
+    this.menuItems = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
           {
-              label: 'New',
-              icon: 'pi pi-fw pi-plus',
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
           },
           {
-              label: 'Delete',
-              icon: 'pi pi-fw pi-trash'
+            label: 'Numeros',
+            icon: 'pi pi-dollar',
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+          },
+        ]
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Otro elemento',
+            icon: 'pi pi-globe',
+            items: [
+              {
+                label: 'Otro elemento',
+                icon: 'pi pi-globe',
+                
+              }
+            ]
           }
-      ];
+        ]
+      }
+    ];
   }
 
 }
